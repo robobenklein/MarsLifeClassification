86131813d284112fe91ef23b0e4d22bd252f32ab
robobenklein <robobenklein@gmail.com>
2015-03-30 13:36:59 -0400

    Added extra info abilities

--- src/classification/Creature.java
+++ src/classification/Creature.java
@@ -1,16 +1,41 @@
 package classification;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Creature extends Ingesting {
     
     String genus;
     String species;
+    Map<String, String> information = new HashMap<String, String>();
     
+    // Constructor without extra info
     public Creature(String motive, String ingestion, String genus, String species) {
+        super(motive, ingestion);
+        
+        this.genus = genus;
+        this.species = species;
+    }
+    // Constructor with optional extra info
+    public Creature(String motive, String ingestion, String genus, String species, Map<String, String> information) {
         // TODO Auto-generated constructor stub
         super(motive, ingestion);
         
         this.genus = genus;
         this.species = species;
+        this.information = information;
+    }
+    // Adds extra information to the Creature's information dict.
+    public void addInfo(String key, String value) {
+        information.put(key, value);
+    }
+    // Returns information from dict if key is present
+    public String getInfo(String key) {
+        try {
+            return information.get(key);
+        } catch (Exception e) {
+            return "Error getting information.";
+        }
     }
     
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,25 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 import classification.Creature;
 
 public class Main {
     
+    private static Random randomGenerator;
+    
     public static void main(String[] args) {
+
+        randomGenerator = new Random();
+        
+        List<Creature> creatures = new ArrayList<Creature>();
+        for (int i=0; i<20; i++) {
+            getRandomMotive();
+        }
+        creatures.add(new Creature("repens", "perficiendo", "bardicus", "lichens"));
         
         System.out.println();
         
@@ -14,6 +28,27 @@ public class Main {
         
         
         scan.close();
+    }
+
+    // Returns a random Motive
+    private static String getRandomMotive() {
+        List<String> choices = new ArrayList<String>(Arrays.asList(
+                                                                   "pendens", //Floating
+                                                                   "repens", //Crawling
+                                                                   "perfodiens", //Burrowing
+                                                                   "sedens" //Sedentary (not moving)
+                                                                   ));
+        int index = randomGenerator.nextInt(choices.size());
+        return choices.get(index);
+    }
+    private static String getRandomIngest() {
+        List<String> choices = new ArrayList<String>(Arrays.asList(
+                                                                   "vaporem", //Gaseous consumption
+                                                                   "carnem", //Carnivore
+                                                                   "perficiendo" //Processing
+                                                                   ));
+        int index = randomGenerator.nextInt(choices.size());
+        return choices.get(index);
     }
     
 }
