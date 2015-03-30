9c94dcfc677df73eb45acb3e60b36097308acd58
robobenklein <robobenklein@gmail.com>
2015-03-30 10:14:18 -0400

    Renamed from package `ui` to `main`

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
-package ui;
+package main;
 
 import java.util.Scanner;
 
 public class Main {
     
     public static void main(String[] args) {
+        
+        System.out.println();
         
         Scanner scan = new Scanner(System.in);
         
